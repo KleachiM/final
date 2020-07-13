1de871bea1ed5292afb5bb797e598bf50259ec00
Mikhail Kalinin <werf121@rambler.ru>
2020-07-13 22:59:36 +0400

    13/07/20 21:59

---
+++ main.py
@@ -0,0 +1,38 @@
+import requests
+from urllib.parse import urlencode
+
+from pprint import pprint
+
+OAUTH_URL = 'https://oauth.vk.com/authorize'
+APP_ID = 7536762
+
+# OAUTH_DATA = {
+#     'client_id': APP_ID,
+#     'display': 'mobile',
+#     'scope': 'photos, offline',
+#     'response_type': 'token',
+#     'v': 5.120,
+# }
+#
+# print('?'.join(
+#     (OAUTH_URL, urlencode(OAUTH_DATA))
+# ))
+
+
+token = '510d2f3bd935bc86e8df8caf31c9b72b4609f6f327df7cb62ece43d3115f8e1ccc3893551a36b2f4b83b0'
+
+response = requests.get(
+    'https://api.vk.com/method/photos.get',
+    params={
+        'owner_id': 4609665,
+        'album_id': 'profile',
+        'extended': 1,
+        'access_token': token,
+        'v': 5.21,
+        'rev': 0,
+        # 'photo_sizes': 1
+    }
+)
+
+print(response)
+pprint(response.json())
