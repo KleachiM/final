5d14477667d82754be6d7ef33c8cdf70d823714f
KleachiM <65865966+KleachiM@users.noreply.github.com>
2020-08-09 22:39:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import os
 from urllib.parse import urlencode
 from datetime import *
+from tqdm import tqdm
 
 from pprint import pprint
 
@@ -21,15 +22,17 @@ from pprint import pprint
 #     (OAUTH_URL, urlencode(OAUTH_DATA))
 # ))
 
-TOKEN = '510d2f3bd935bc86e8df8caf31c9b72b4609f6f327df7cb62ece43d3115f8e1ccc3893551a36b2f4b83b0'
 ID = 4609665
+YA_TOKEN = 'AgAAAAA86I9wAADLW4TMFaX010u7pssNYZgHB8w'
+
+VK_TOKEN = '510d2f3bd935bc86e8df8caf31c9b72b4609f6f327df7cb62ece43d3115f8e1ccc3893551a36b2f4b83b0'
 response = requests.get(
     'https://api.vk.com/method/photos.get',
     params={
         'owner_id': ID,
         'album_id': 'profile',
         'extended': 1,
-        'access_token': TOKEN,
+        'access_token': VK_TOKEN,
         'v': 5.21,
         'rev': 0,
         'photo_sizes': 1
@@ -92,8 +95,8 @@ for photo in json_data:
     else:
         likes_count_tmp.setdefault(photo['likes'], 1)
 
-path = os.path.abspath(os.curdir)
-os.mkdir(os.path.join(path, 'tmp_photos'))
+path = os.path.join(os.path.abspath(os.curdir), 'tmp_photos')
+os.mkdir(path)
 
 for photo in json_data:
     if likes_count_tmp[photo['likes']] > 1:
@@ -101,9 +104,28 @@ for photo in json_data:
     else:
         photo['file_name'] = f'{photo["likes"]}.jpg'
     dnld = requests.get(photo['link'])
-    path = f'tmp_photos/{photo["file_name"]}'
-    with open(path, 'wb') as f:
+    f_path = f'tmp_photos/{photo["file_name"]}'
+    with open(f_path, 'wb') as f:
         f.write(dnld.content)
+
+likes_count_tmp.clear()
+
+params = {'path': 'photos_from_vk'}
+headers = {'Authorization': f'OAuth {YA_TOKEN}'}
+ya_resp = requests.put('https://cloud-api.yandex.net:443/v1/disk/resources', params=params, headers=headers)
+
+for file in os.listdir(path):
+    file_name = file
+
+    params = {'path': f'/photos_from_vk/{file_name}', 'overwrite': 'true'}
+    upl_url = 'https://cloud-api.yandex.net:443/v1/disk/resources/upload'
+
+    ya_resp = requests.get(upl_url, params=params, headers=headers)
+    put_url = ya_resp.json().get('href')
+
+    print(os.path.join(path, file_name))
+    files = {'file': open(os.path.join(path, file_name), 'rb')}
+    ya_resp = requests.put(put_url, files=files)
 
 with open('data.json', 'w') as f:
         json.dump(json_data, f, indent=4)
